8bbf473764c7562f3a5b323f6cd42ac04e40b8a6
KhanhVu12 <ankain1210@gmail.com>
2022-05-05 09:51:11 +0700

    update

--- front-end/scripts/seatSelect.js
+++ front-end/scripts/seatSelect.js
@@ -14,31 +14,33 @@ const seats = document.querySelectorAll(
 const ticketCount = document.getElementById("count");
 const total = document.getElementById("total");
 
-let ticketPrice = 100000;
-
 const handleSelectSeat = (e) => {
-  const isSelected = e.target.className.includes("selected");
-  const isSeatVip = e.target.className.includes("vip");
+  if (e.currentTarget.className.includes("occupied")) return;
+  const isSelected = e.currentTarget.className.includes("selected");
+  const isSeatVip = e.currentTarget.className.includes("vip");
 
   if (isSelected) {
     total.innerHTML =
       +total.textContent -
-      (e.target.className.includes("vip") ? +SeatPrice.VIP : +SeatPrice.NORMAL);
+      (e.currentTarget.className.includes("vip")
+        ? +SeatPrice.VIP
+        : +SeatPrice.NORMAL);
   } else {
     total.innerHTML =
       +total.textContent + (isSeatVip ? +SeatPrice.VIP : +SeatPrice.NORMAL);
   }
 
   if (
-    e.target.classList.contains("seat") &&
-    !e.target.classList.contains("occupied")
+    e.currentTarget.classList.contains("seat") &&
+    !e.currentTarget.classList.contains("occupied")
   ) {
-    e.target.classList.toggle("selected");
+    e.currentTarget.classList.toggle("selected");
   }
 
   ticketCount.textContent = seatContainer.querySelectorAll(".selected").length;
 };
 
-seatContainer.addEventListener("click", (e) => {
-  handleSelectSeat(e);
-});
+
+seats.forEach((element) => {
+  element.addEventListener("click", handleSelectSeat)
+})
